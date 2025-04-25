fe7377d1d2ddfa8d091ae036517412cdbcde64a6
LeonidasZoidis <lzoidis123@gmail.com>
2025-04-25 14:36:15 +0100

    additions

--- heading.js
+++
@@ -1,6 +0,0 @@
-const heading = () => {
-    const heading = document.createElement('h1');
-    heading.textContent = 'Hello TypeScript!';
-    document.body.appendChild(heading);
-};
-export default heading;
--- main.js
+++
@@ -1,2 +0,0 @@
-import heading from './heading.js';
-heading();
